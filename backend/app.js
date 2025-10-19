f565e376f53ac04790911f69be8e91ea8c4f0d1b
Lia-606 <liapv606@gmail.com>
2025-10-18 20:48:30 -0500

    conexion a la base de datos

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Cargar variables de entorno
+dotenv.config();
+
+// Conectar a MongoDB
+connectDB();
+
+// Inicializar app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// Rutas base (por ahora vacías)
+app.get('/', (req, res) => {
+  res.send('🍽️ API RESTAURANTE MR QUEEN funcionando correctamente');
+});
+
+// Puerto
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor backend corriendo en el puerto ${PORT}`);
+});
